dad9b6eab6e9cc0c20ebb6beee789e6575c80621
pbarcelog <44697031+pbarcelog@users.noreply.github.com>
2018-11-17 10:59:42 +0100

    Update TSAESessionOriginatorSide.java

--- src/recipes_service/tsae/sessions/TSAESessionOriginatorSide.java
+++ src/recipes_service/tsae/sessions/TSAESessionOriginatorSide.java
@@ -52,21 +52,21 @@ import edu.uoc.dpcs.lsim.logger.LoggerManager.Level;
  * December 2012
  *
  */
-public class TSAESessionOriginatorSide extends TimerTask{
+public class TSAESessionOriginatorSide extends TimerTask{ //TimerTask es clase que programa tareas puntuales o recurrentes
 	// Needed for the logging system sgeag@2017
-	private LSimWorker lsim = LSimFactory.getWorkerInstance();
-	private static AtomicInteger session_number = new AtomicInteger(0);
+	private LSimWorker lsim = LSimFactory.getWorkerInstance(); /* LSim es todo un tema por su cuenta. En principio asumir que es una parte del código en la que no entrar */
+	private static AtomicInteger session_number = new AtomicInteger(0); // Se intenta que el número de sesión sea único
 	
 	private ServerData serverData;
-	public TSAESessionOriginatorSide(ServerData serverData){
-		super();
+	public TSAESessionOriginatorSide(ServerData serverData){ // Función muy sencilla para asignar datos del servidor que solicitará la sesión AE
+		super(); // Para qué llama al constructor de la superclase?
 		this.serverData=serverData;		
 	}
 	
 	/**
 	 * Implementation of the TimeStamped Anti-Entropy protocol
 	 */
-	public void run(){
+	public void run(){ // Ejecución de la sesión AE; detalles más abajo. ¿Quién invoca esta rutina?
 		sessionWithN(serverData.getNumberSessions());
 	}
 
@@ -78,9 +78,9 @@ public class TSAESessionOriginatorSide extends TimerTask{
 	public void sessionWithN(int num){
 		if(!SimulationData.getInstance().isConnected())
 			return;
-		List<Host> partnersTSAEsession= serverData.getRandomPartners(num);
+		List<Host> partnersTSAEsession= serverData.getRandomPartners(num); /* Lista de los servers en orden aleatorio. En clase Hosts del paquete recipes_service.communication */
 		Host n;
-		for(int i=0; i<partnersTSAEsession.size(); i++){
+		for(int i=0; i<partnersTSAEsession.size(); i++){  /* Ejectua la sesión AE servidor tras servidor en el orden aleatorio devuelto por la función getRandomPartners. Para los detalles de la sesión hay que ir a la función SessionTSAE */
 			n=partnersTSAEsession.get(i);
 			sessionTSAE(n);
 		}
@@ -92,13 +92,13 @@ public class TSAESessionOriginatorSide extends TimerTask{
 	 * @param n
 	 */
 	private void sessionTSAE(Host n){
-		int current_session_number = session_number.incrementAndGet();
+		int current_session_number = session_number.incrementAndGet(); // autoexplicativo
 		if (n == null) return;
 		
 		lsim.log(Level.TRACE, "[TSAESessionOriginatorSide] [session: "+current_session_number+"] TSAE session");
 
 		try {
-			Socket socket = new Socket(n.getAddress(), n.getPort());
+			Socket socket = new Socket(n.getAddress(), n.getPort()); //Establece canal de comunicación del server partner con server que inicia la AE
 			ObjectInputStream_DS in = new ObjectInputStream_DS(socket.getInputStream());
 			ObjectOutputStream_DS out = new ObjectOutputStream_DS(socket.getOutputStream());
 
@@ -106,27 +106,51 @@ public class TSAESessionOriginatorSide extends TimerTask{
 			TimestampMatrix localAck = null;
 			
 			// Send to partner: local's summary and ack
+			/* ¿Cómo se asignan valores a las variables localSummary y localAck? La información está en serverData y las funciones en ServerData. 
+			 * Habría que hacer:
+			 * 
+			 *  localSummary = serverData.getSummary();
+			 *  localAck= serverData.getAck(); En principio el Ack estará vacío, la clase TimestampMatrix se ha de acabar de programar, 
+			 *  pero no parece relevante para la Phase2: el ACK se usa con el Purge, que no hacemos aquí
+			 *  
+			 */
 			Message	msg = new MessageAErequest(localSummary, localAck);
 			msg.setSessionNumber(current_session_number);
-            out.writeObject(msg);
+            		out.writeObject(msg);
 			lsim.log(Level.TRACE, "[TSAESessionOriginatorSide] [session: "+current_session_number+"] sent message: "+msg);
 
             // receive operations from partner
+		/* ¿Cómo se 'activa' el partner para poner en marcha el run() del TSAESessionPartnerSide? No tengo ni idea, asumiré que funciona */
 			msg = (Message) in.readObject();
 			lsim.log(Level.TRACE, "[TSAESessionOriginatorSide] [session: "+current_session_number+"] received message: "+msg);
 			while (msg.type() == MsgType.OPERATION){
-				// ...
+				/* Implementar update del log con los nuevos mensajes que se van recibiendo del partner de la misma manera que en la práctica 1. 
+				 * En principio algo del estilo:
+				 * 
+				 *  	this.log.add(op);
+				 *	this.summary.updateTimestamp(timestamp); otra opción es hacer un updateMax al final de la recepción de mensajes, 
+				 *      pero sería necesario un summary vacío que se llenara con los mensajes 
+				 *	this.recipes.add(rcpe) o this.recipes.remove(rcpe) en función de la operación (remove todavía no se ha implementado);
+				 *  
+				 */
 				msg = (Message) in.readObject();
 				lsim.log(Level.TRACE, "[TSAESessionOriginatorSide] [session: "+current_session_number+"] received message: "+msg);
 			}
 
             // receive partner's summary and ack
 			if (msg.type() == MsgType.AE_REQUEST){
-				// ...
-				
-				// send operations
-				
-				//...
+				/* 
+				 * Copia local de summary y ack para evitar problemas de concurrencia.
+				 * TimestampVector localSummary = msg.getSummary();
+				 * TimestampMatrix localAck = msg.getAck();
+				 * 
+				 * Ahora hay que comparar los summaries para ver qué se tiene de más que el Partner
+				 * 	Conseguir lista de todos los participants
+				 * 	Hacer un FOR que compare para cada Server el summary del Partner con el summary del Originator, 
+				 *	comprobar si la función log.listNewer nos ahorra la faena
+				 * 	Si el Summary del Partner > Summary del Originator, enviar las operaciones al Originator, construyendo el msg
+				 *
+				 * send operations */
 					msg.setSessionNumber(current_session_number);
 					out.writeObject(msg);
 					lsim.log(Level.TRACE, "[TSAESessionOriginatorSide] [session: "+current_session_number+"] sent message: "+msg);
@@ -134,7 +158,7 @@ public class TSAESessionOriginatorSide extends TimerTask{
 				// send and "end of TSAE session" message
 				msg = new MessageEndTSAE();  
 				msg.setSessionNumber(current_session_number);
-	            out.writeObject(msg);					
+	            		out.writeObject(msg);					
 				lsim.log(Level.TRACE, "[TSAESessionOriginatorSide] [session: "+current_session_number+"] sent message: "+msg);
 
 				// receive message to inform about the ending of the TSAE session
